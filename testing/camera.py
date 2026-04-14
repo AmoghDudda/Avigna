4aa74ba009723381a8fee598474a17b577d973a5
AmoghDudda <dudda.amogh@gmail.com>
2026-04-13 19:19:15 -0700

    day1 complete and fixed

--- testing/camera.py
+++ testing/camera.py
@@ -1,20 +1,17 @@
 import cv2  
 import time
-
 cap=cv2.VideoCapture(0)
 #cap=cv2.VideoCapture("esp32 ka ip address")
 last_time = time.time()
 while True:
-    
-   
     ret,frame=cap.read()
-    if ret:
-        cv2.imshow("Avigna",frame)
-
-        if time.time() - last_time >= 4:
-            print(f"Shape: {frame.shape[1]}")
-            last_time = time.time()
-        if cv2.waitKey(1) & 0xFF==ord('q'):
-            break
+    if not ret:
+        break
+    cv2.imshow("Avigna",frame)
+    if time.time() - last_time >= 1:
+        print(f"Shape: {frame.shape}")
+        last_time = time.time()
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
 cap.release()
 cv2.destroyAllWindows()
